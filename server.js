66855121117cd8115c4593417d59e5ad04fa7b03
sabbaghh915 <128121728+sabbaghh915@users.noreply.github.com>
2025-05-24 16:49:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -24,5 +24,5 @@ app.use('/api/upload', require('./routes/uploadRoutes'));
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
